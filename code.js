d57614d60f15014b94e6e58b7d52fd8ddb682444
Izy266 <islamelkattawy@gmail.com>
2023-10-01 23:56:57 -0400

    Added shader button and behaviour

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 let boardSize = document.querySelector(".slider");
 let colorful = false;
 let erase = false;
+let shade = false;
 
 drawBoard(boardSize.value);
 drawSquares();
@@ -14,6 +15,8 @@ function drawBoard(size) {
         board.appendChild(row);
         for (let j = 0; j < size; j++) {
             square = document.createElement("div");
+            square.setAttribute("shadeVal", "0");
+            square.style.background = "rgb(255, 255, 255)";
             square.className = "square";
             row.appendChild(square);
         }
@@ -24,6 +27,7 @@ function drawBoard(size) {
 function clearModes() {
     colorful = false;
     erase = false;
+    shade = false;
 }
 
 function drawSquares() {
@@ -38,7 +42,7 @@ function drawSquares() {
             color = false;
         }
 
-        square.addEventListener("mousemove", () => {
+        square.addEventListener("mouseover", () => {
             if (color) {
                 if (colorful) {
                     square.style.background = `rgb(
@@ -48,6 +52,23 @@ function drawSquares() {
                     )`;
                 } else if (erase) {
                     square.style.background = board.style.background;
+                } else if (shade) {
+                    shadeVal = square.getAttribute("shadeVal");
+                    if (shadeVal < 10) {
+                        square.setAttribute("shadeVal", ++shadeVal);
+                        let rgb = square.style.background;
+                        let rgbList = rgb.substring(4, rgb.length - 1)
+                            .replace(/ /g, '')
+                            .split(',');
+                        console.log(rgbList);
+                        for (let i = 0; i < rgbList.length; i++) {
+                            rgbList[i] = Math.floor(rgbList[i] - rgbList[i]/(11 - shadeVal));
+                            
+                        }
+                        square.style.background = `rgb(${rgbList[0]}, ${rgbList[1]}, ${rgbList[2]})`;
+                        console.log(rgbList);
+                        console.log(shadeVal); 
+                    }
                 } else {
                     square.style.background = document.querySelector("#chooseColor").value;
                 }
